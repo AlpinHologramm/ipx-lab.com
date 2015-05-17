94c50e9d1cf13076467eb49a2a8d4fcef5d92ca3
Alpin <contact@florentlagrange.eu>
2015-05-17 20:18:52 +0200

    Update

---
+++ www/DF/V1/_sandbox/index_bck_05.php
@@ -0,0 +1,228 @@
+<?php
+  session_start();
+  ?>
+  
+  <!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if (IE 9)]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en-US"><html lang="fr-FR"> <!--<![endif]-->
+<head>
+
+<!-- Meta Tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Dire & Faire</title>   
+
+<meta name="description" content="Dire & Faire" /> 
+
+<!-- Mobile Specifics -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="HandheldFriendly" content="true"/>
+<meta name="MobileOptimized" content="320"/>   
+
+<!-- Mobile Internet Explorer ClearType Technology -->
+<!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
+
+<!-- Bootstrap -->
+<link href="_include/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Main Style -->
+<link href="_include/css/main_02.css" rel="stylesheet">
+
+<!-- Supersized -->
+<link href="_include/css/supersized.css" rel="stylesheet">
+<link href="_include/css/supersized.shutter.css" rel="stylesheet">
+
+<!-- FancyBox -->
+<link href="_include/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+
+<!-- Font Icons -->
+<link href="_include/css/fonts.css" rel="stylesheet">
+
+<!-- Shortcodes -->
+<link href="_include/css/shortcodes.css" rel="stylesheet">
+
+<!-- Responsive -->
+<link href="_include/css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="_include/css/responsive.css" rel="stylesheet">
+
+<!-- Supersized -->
+<link href="_include/css/supersized.css" rel="stylesheet">
+<link href="_include/css/supersized.shutter.css" rel="stylesheet">
+
+<!-- Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
+
+<!-- Fav Icon -->
+<link rel="shortcut icon" href="#">
+
+<link rel="apple-touch-icon" href="#">
+<link rel="apple-touch-icon" sizes="114x114" href="#">
+<link rel="apple-touch-icon" sizes="72x72" href="#">
+<link rel="apple-touch-icon" sizes="144x144" href="#">
+
+<!-- Modernizr -->
+<script src="_include/js/modernizr.js"></script>
+
+<!-- Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'Insert Your Code']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- End Analytics -->
+
+</head>
+
+
+<body>
+
+<!-- This section is for Splash Screen -->
+<div class="ole"></div>
+<!-- End of Splash Screen -->
+
+
+<!-- Home Section -->
+<div id="contact" class="page">
+<div class="container">
+   
+    <!-- Title Page -->
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">Dire & Faire <br/></p>
+            </div>
+        </div>
+    </div>
+    <!-- End Title Page -->
+    
+    <!-- Contact Form -->
+    
+    <div style="text-align:center;">
+    <div class="row">
+
+                <p class="contact-email">
+                  </form>
+        </p>
+        <p><br/>Oublier les obligations sociales. Rencontrer d'autres personnes.</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+    </div>    
+    </div>
+    <!-- End Contact Form -->
+</div>
+</div>
+<!-- End Home Section -->
+
+
+
+
+
+<!-- Transition Section -->
+<div id="about" class="page-alternate">
+<div class="container">
+    <!-- Title Page -->
+    <div class="row">
+      <div class="span12">
+        <div class="title-page">
+            
+            <?php include("contact.php"); ?>
+            
+            
+                <h2 style="color:white;" class="title-description"><br/><br/><br/> 
+                Bient&ocirc;t, vous allez pouvoir participer à une exp&eacute;rience artistique qui vous donnera la possibilit&eacute; de rencontrer <br/> 
+                ceux et celles &agrave; qui vous ne parlez pas, habituellement. </h2>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                
+          </div>
+   	    <p>&nbsp;</p>
+   	    <p>&nbsp;</p>
+   	    <p>&nbsp;</p>
+        </div>
+     </div>
+     <!-- End Title Page -->
+</div>
+</div> 
+<!-- End Transition Section -->        
+                
+                
+                
+                
+
+<!-- About Section -->
+
+<div id="about" class="page">
+<div class="container">
+
+    <!-- Title Page -->
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">                
+				<h3 class="title-description">D&egrave;s que le site sera publi&eacute;, et apr&egrave;s avoir renseign&eacute; votre adresse, vous recevrez un mail qui vous permettra d'acc&eacute;der &agrave; ses fonctionnalit&eacute;s avanc&eacute;es : </h3>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+				<p>&nbsp;</p>
+          </div>
+        </div>
+    </div>
+    <!-- End Title Page -->
+    
+    
+    
+    <!-- Modules --><!-- End Modules -->
+</div>
+</div>
+<!-- End About Section -->
+
+
+
+<!-- Back To Top -->
+<a id="back-to-top" href="#">
+	<i class="font-icon-arrow-simple-up"></i>
+</a>
+<!-- End Back to Top -->
+
+
+<!-- Js -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <!-- jQuery Core -->
+<script src="_include/js/bootstrap.min.js"></script> <!-- Bootstrap -->
+<script src="_include/js/supersized.3.2.7.min.js"></script> <!-- Slider -->
+<script src="_include/js/waypoints.js"></script> <!-- WayPoints -->
+<script src="_include/js/waypoints-sticky.js"></script> <!-- Waypoints for Header -->
+<script src="_include/js/jquery.isotope.js"></script> <!-- Isotope Filter -->
+<script src="_include/js/jquery.fancybox.pack.js"></script> <!-- Fancybox -->
+<script src="_include/js/jquery.fancybox-media.js"></script> <!-- Fancybox for Media -->
+<script src="_include/js/jquery.tweet.js"></script> <!-- Tweet -->
+<script src="_include/js/plugins.js"></script> <!-- Contains: jPreloader, jQuery Easing, jQuery ScrollTo, jQuery One Page Navi -->
+<script src="_include/js/main.js"></script> <!-- Default JS -->
+<!-- End Js -->
+
+</body>
+</html>
---
+++ www/DF/V1/mail.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if (IE 9)]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en-US"><html lang="fr-FR"> <!--<![endif]-->
+<head>
+
+<!-- Meta Tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Dire & Faire</title>   
+
+<meta name="description" content="Dire & Faire" /> 
+
+<!-- Mobile Specifics -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="HandheldFriendly" content="true"/>
+<meta name="MobileOptimized" content="320"/>   
+
+<!-- Mobile Internet Explorer ClearType Technology -->
+<!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
+
+<!-- Bootstrap -->
+<link href="_include/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Main Style -->
+<link href="_include/css/main_02.css" rel="stylesheet">
+
+<!-- Supersized -->
+<link href="_include/css/supersized.css" rel="stylesheet">
+<link href="_include/css/supersized.shutter.css" rel="stylesheet">
+
+<!-- FancyBox -->
+<link href="_include/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+
+<!-- Font Icons -->
+<link href="_include/css/fonts.css" rel="stylesheet">
+
+<!-- Shortcodes -->
+<link href="_include/css/shortcodes.css" rel="stylesheet">
+
+<!-- Responsive -->
+<link href="_include/css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="_include/css/responsive.css" rel="stylesheet">
+
+<!-- Supersized -->
+<link href="_include/css/supersized.css" rel="stylesheet">
+<link href="_include/css/supersized.shutter.css" rel="stylesheet">
+
+<!-- Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900' rel='stylesheet' type='text/css'>
+
+<!-- Fav Icon -->
+<link rel="shortcut icon" href="#">
+
+<link rel="apple-touch-icon" href="#">
+<link rel="apple-touch-icon" sizes="114x114" href="#">
+<link rel="apple-touch-icon" sizes="72x72" href="#">
+<link rel="apple-touch-icon" sizes="144x144" href="#">
+
+<!-- Modernizr -->
+<script src="_include/js/modernizr.js"></script>
+
+<!-- Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'Insert Your Code']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- End Analytics -->
+
+</head>
+
+
+<body>
+
+
+
+
+<!-- Home Section -->
+<div id="contact" class="page">
+<div class="container">
+   
+    <!-- Title Page -->
+  <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+              <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">Dire & Faire</p>
+              <p id="sous-titre" style="font-size:19px; color:#000; font-weight:100; line-height:normal; font-family: Arial, Helvetica, sans-serif;">Oublier les obligations sociales. Rencontrer d'autres personnes.</p>
+            <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;"><br/>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- End Title Page -->
+    
+        
+
+
+<!-- Contact Form -->
+
+
+
+ <div class="row">
+        <div class="span12"></div>
+    </div>
+
+    
+
+    
+
+    <!-- End Contact Form -->
+</div>
+</div>
+<!-- End Home Section -->
+
+
+
+
+
+
+
+
+<!-- Transition Section -->
+<div id="about" class="page-alternate">
+<div class="container">
+    <!-- Title Page -->
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <h2 style="color:white;" class="title-description"><br/><br/><br/>
+                  <?php include("contact.php"); ?>
+                </h2>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                
+            </div>
+    	</div>
+     </div>
+     <!-- End Title Page -->
+</div>
+</div> 
+<!-- End Transition Section -->      
+
+      
+                
+            <!-- Title Page -->
+            <div id="contact2" class="page">
+              <div class="container">
+                <!-- Title Page -->
+                <div class="row">
+                  <div class="span12">
+                    <div class="title-page">
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      D&egrave;s que le site sera publi&eacute;, et apr&egrave;s avoir renseign&eacute; votre adresse,</span></p>
+                   vous recevrez un mail qui vous permettra d'acc&eacute;der &agrave; ses fonctionnalit&eacute;s avanc&eacute;es : </span></p>
+<p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;">&nbsp;</p>
+                      <p style="font-size:100px; color:#999; font-weight:100; line-height:normal; font-family: 'Arial Black', Gadget, sans-serif;"><br/>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Title Page -->
+                <!-- Contact Form -->
+                <div class="row">
+                  <div class="span12"></div>
+                </div>
+                <!-- End Contact Form -->
+              </div>
+            </div>
+            <!-- End Title Page -->      
+                
+           
+<!-- Transition Section -->
+<div id="about" class="page-alternate">
+<div class="container">
+    <!-- Title Page -->
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <h2 style="color:white;" class="title-description"><br/><br/><br/></h2>
+                <div class="row">
+                  <!-- Start Module 01 -->
+                  <div class="span4 profile">
+                    <div class="image-wrap">
+                      <div class="hover-wrap"> <span class="overlay-img"></span></div>
+                      <img src="_include/img/profile/profile-01.jpg" alt="John Doe"></div>
+                    <h3 class="profile-name">Partition</h3>
+                    <p class="profile-description">Composer une partition avec une écriture collaborative.</p>
+                    <div class="social">
+                      <ul class="social-icons">
+                        <li><a href="#"><i class="font-icon-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="font-icon-social-dribbble"></i></a></li>
+                        <li><a href="#"><i class="font-icon-social-facebook"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- End Module 01 -->
+                  <!-- Start Module 02 -->
+                  <div class="span4 profile">
+                    <div class="image-wrap">
+                      <div class="hover-wrap"> <span class="overlay-img"></span></div>
+                      <img src="_include/img/profile/profile-02.jpg" alt="Jane Helf"></div>
+                    <h3 class="profile-name">Interprétation</h3>
+                    <p class="profile-description">L'interpréter et produire des relations uniques.</p>
+                    <div class="social">
+                      <ul class="social-icons">
+                        <li><a href="#"><i class="font-icon-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="font-icon-social-email"></i></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- End Module 02 -->
+                  <!-- Start Module 03 -->
+                  <div class="span4 profile">
+                    <div class="image-wrap">
+                      <div class="hover-wrap"> <span class="overlay-img"></span></div>
+                      <img src="_include/img/profile/profile-03.jpg" alt="Joshua Insanus"></div>
+                    <h3 class="profile-name">Témoignages</h3>
+                    <p class="profile-description">Publier votre témoignage, et accéder à celui des autres.</p>
+                  </div>
+                  <!-- End Module 03 -->
+                </div>
+<p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                <p style="color:white;">&nbsp;</p>
+                
+            </div>
+    	</div>
+     </div>
+     <!-- End Title Page -->
+</div>
+</div> 
+<!-- End Transition Section -->           
+
+<!-- About Section -->
+
+<div id="about" class="page"></div>
+<!-- End About Section -->
+
+
+
+<!-- Back To Top -->
+<a id="back-to-top" href="#">
+	<i class="font-icon-arrow-simple-up"></i>
+</a>
+<!-- End Back to Top -->
+
+
+<!-- Js -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <!-- jQuery Core -->
+<script src="_include/js/bootstrap.min.js"></script> <!-- Bootstrap -->
+<script src="_include/js/supersized.3.2.7.min.js"></script> <!-- Slider -->
+<script src="_include/js/waypoints.js"></script> <!-- WayPoints -->
+<script src="_include/js/waypoints-sticky.js"></script> <!-- Waypoints for Header -->
+<script src="_include/js/jquery.isotope.js"></script> <!-- Isotope Filter -->
+<script src="_include/js/jquery.fancybox.pack.js"></script> <!-- Fancybox -->
+<script src="_include/js/jquery.fancybox-media.js"></script> <!-- Fancybox for Media -->
+<script src="_include/js/jquery.tweet.js"></script> <!-- Tweet -->
+<script src="_include/js/plugins.js"></script> <!-- Contains: jPreloader, jQuery Easing, jQuery ScrollTo, jQuery One Page Navi -->
+<script src="_include/js/main.js"></script> <!-- Default JS -->
+<!-- End Js -->
+
+</body>
+</html>
